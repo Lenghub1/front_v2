6c619b14fc00e232fe59a776a08b1257ce2c9372
Tourleng <hourleng9@gmail.com>
2023-11-07 15:49:39 +0700

    notification buy

--- src/Notif.jsx
+++ src/Notif.jsx
@@ -1,44 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
 
 const SocketTest = () => {
-  const [notification, setNotification] = useState('');
+  const [notifications, setNotifications] = useState([]);
+  const [showNotifications, setShowNotifications] = useState(false);
 
   useEffect(() => {
-    const socket = io("http://localhost:4000/"); // Replace with your server URL
+    const socket = io("http://localhost:4000"); // Replace with your server URL
 
-    socket.on('connect', () => {
-      console.log('Connected to the server via WebSocket');
+    socket.on("connect", () => {
+      console.log("Connected to the server via WebSocket");
     });
 
-    socket.on('disconnect', () => {
-      console.log('Disconnected from the server via WebSocket');
+    socket.on("disconnect", () => {
+      console.log("Disconnected from the server via WebSocket");
     });
 
-    socket.on('newOrder', (data) => {
-      // When a new notification is received, update the notification state
-      setNotification(data.message);
-      console.log("new order", data);
-    });
+    socket.on("orderStored", (data) => {
+        console.log("Product stored:", data.message);
+        setNotifications((prevNotifications) => [...prevNotifications, data.message]);
+        console.log("Notifications:", notifications);
+      });
+      
 
     return () => {
       socket.disconnect();
     };
   }, []);
 
+  const toggleNotifications = () => {
+    setShowNotifications(!showNotifications);
+  };
+
   return (
     <div>
       <h2>Socket.IO Notifications</h2>
-      {notification ? (
-        <div>
-          <p>Notification:</p>
-          <p>{notification}</p>
-        </div>
-      ) : (
-        <p>No notifications yet.</p>
-      )}
+      <div className={`noti ${showNotifications ? 'show-notifications' : ''}`} onClick={toggleNotifications}>
+        <i className="fa-regular fa-bell"></i>
+        <ul className="notification-list">
+          {notifications.map((notification, index) => (
+            <li key={index}>{notification}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-};
-
+}
 export default SocketTest;

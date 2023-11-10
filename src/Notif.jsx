414d58fa8c7a8e2683d47225a6e88611561baf36
Tourleng <hourleng9@gmail.com>
2023-11-10 20:11:24 +0700

    new order

--- src/Notif.jsx
+++ src/Notif.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 const SocketTest = () => {
-  const [notifications, setNotifications] = useState([]);
-  const [showNotifications, setShowNotifications] = useState(false);
+  const [buyerNotifications, setBuyerNotifications] = useState([]);
+  const [sellerNotifications, setSellerNotifications] = useState([]);
+  const [showBuyerNotifications, setShowBuyerNotifications] = useState(false);
+  const [showSellerNotifications, setShowSellerNotifications] = useState(false);
 
   useEffect(() => {
     const socket = io("http://localhost:4000"); // Replace with your server URL
@@ -16,10 +18,14 @@ const SocketTest = () => {
       console.log("Disconnected from the server via WebSocket");
     });
 
-    socket.on("orderStored", (data) => {
+    socket.on("buyerNotification", (data) => {
       console.log("Product stored:", data.message);
-      setNotifications((prevNotifications) => [...prevNotifications, data.message]);
-      console.log("Notifications:", notifications);
+      setBuyerNotifications((prevNotifications) => [...prevNotifications, data.message]);
+    });
+
+    socket.on("sellerNotification", (data) => {
+      console.log("Seller notification:", data.message);
+      setSellerNotifications((prevNotifications) => [...prevNotifications, data.message]);
     });
 
     return () => {
@@ -27,29 +33,52 @@ const SocketTest = () => {
     };
   }, []);
 
-  const toggleNotifications = () => {
-    setShowNotifications(!showNotifications);
+  const toggleBuyerNotifications = () => {
+    setShowBuyerNotifications(!showBuyerNotifications);
+  };
+
+  const toggleSellerNotifications = () => {
+    setShowSellerNotifications(!showSellerNotifications);
   };
 
   return (
     <div>
       <h2>Socket.IO Notifications</h2>
-      <div className={`noti ${showNotifications ? 'show-notifications' : ''}`} onClick={toggleNotifications}>
-        {showNotifications ? (
+      <div className={`noti ${showBuyerNotifications ? 'show-notifications' : ''}`} onClick={toggleBuyerNotifications}>
+        {showBuyerNotifications ? (
           <i className="fa-solid fa-bell"></i>
         ) : (
           <div style={{ position: "relative" }}>
             <i className="fa-regular fa-bell"></i>
-            {notifications.length > 0 && (
+            {buyerNotifications.length > 0 && (
               <div className="notification-dot"></div>
             )}
           </div>
         )}
         <ul className="notification-list">
-          {notifications.map((notification, index) => (
+          {buyerNotifications.map((notification, index) => (
             <li key={index}>{notification}</li>
           ))}
         </ul>
+      </div>
+      <div className="sellernoti">
+        <div className={`noti ${showSellerNotifications ? 'show-notifications' : ''}`} onClick={toggleSellerNotifications}>
+          {showSellerNotifications ? (
+            <i className="fa-solid fa-bell"></i>
+          ) : (
+            <div style={{ position: "relative" }}>
+              <i className="fa-regular fa-bell"></i>
+              {sellerNotifications.length > 0 && (
+                <div className="notification-dot"></div>
+              )}
+            </div>
+          )}
+          <ul className="notification-list">
+            {sellerNotifications.map((notification, index) => (
+              <li key={index}>{notification}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -6,6 +6,7 @@ const Chatbot = () => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [showEmptyInputWarning, setShowEmptyInputWarning] = useState(false);
+  const [productCardRendered, setProductCardRendered] = useState(false);
   const chatMessagesRef = useRef(null);
 
   const scrollToBottom = () => {
@@ -34,12 +35,76 @@ const Chatbot = () => {
         }
       );
 
+      const intentResponse = response.data.intent.displayName;
       const botResponse = response.data.fulfillmentText;
+
       setMessages((prevMessages) => [
         ...prevMessages,
         { who: "bot", text: botResponse },
       ]);
-      scrollToBottom();
+
+      if (intentResponse === "show.product") {
+        // Reset the productCardRendered state to false
+        setProductCardRendered(false);
+
+        const products = [
+          {
+            name: "Mango Tree",
+            price: `$${(Math.random() * 50).toFixed(2)}`,
+            seller: "ah sour",
+          },
+          {
+            name: "Mango",
+            price: `$${(Math.random() * 20).toFixed(2)}`,
+            seller: "ah chak",
+          },
+          {
+            name: "Sun Flower",
+            price: `$${(Math.random() * 30).toFixed(2)}`,
+            seller: "ah poo",
+          },
+          {
+            name: "Cacao Tree",
+            price: `$${(Math.random() * 40).toFixed(2)}`,
+            seller: "ah jm tou",
+          },
+          {
+            name: "Happy Leaf",
+            price: `$${(Math.random() * 10).toFixed(2)}`,
+            seller: "Leng",
+          },
+        ];
+
+        const productElements = products.map((product, index) => (
+          <div key={index} className="product-card">
+            <div className="product-name">Name: {product.name}</div>
+            <div>
+              <img
+                src="https://tse4.mm.bing.net/th?id=OIP.0rdnycv60_W_KXagEX5tHwHaHa&pid=Api&P=0&h=220"
+                alt={product.name}
+              />
+            </div>
+            <p>owner: {product.seller}</p>
+            <div>Price: {product.price}</div>
+            <div className="button-card">
+              <button>Buy</button>
+              <button>view</button>
+            </div>
+          </div>
+        ));
+
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          {
+            text: <div className="product-container">{productElements}</div>,
+          },
+        ]);
+
+        setProductCardRendered(true);
+        scrollToBottom();
+      }
+
+     
       setInput("");
     } catch (error) {
       const errorMessage = "Error, please check your request";
@@ -58,9 +123,35 @@ const Chatbot = () => {
     }
   };
 
+  const handleButtonClick = (buttonText) => {
+    textQuery(buttonText);
+    setInput("");
+  };
+
+  const renderNewOrderButton = () => {
+    if (
+      messages.length > 0 &&
+      messages[messages.length - 1].text === "Please make a new order. 🤓"
+    ) {
+      return (
+        <div className="new-order-btn">
+          <button onClick={() => handleButtonClick("i want to make new order")}>
+            Make a New Order
+          </button>
+        </div>
+      );
+    }
+    return null;
+  };
+
   useEffect(() => {
     scrollToBottom();
-  }, [messages]);
+  }, [messages, productCardRendered]);
+
+  useEffect(() => {
+    const welcomeMessage = "Welcome! How can I assist you today?";
+    setMessages([{ who: "bot", text: welcomeMessage, special: true }]);
+  }, []);
 
   return (
     <div className="chatbot">
@@ -70,6 +161,7 @@ const Chatbot = () => {
             <div>{message.text}</div>
           </div>
         ))}
+        {renderNewOrderButton()}
       </div>
       {showEmptyInputWarning && (
         <div className="empty-input-warning">Please enter a message.</div>
@@ -82,6 +174,37 @@ const Chatbot = () => {
           onChange={(e) => setInput(e.target.value)}
           placeholder="Type a message..."
         />
+
+        {messages.length > 0 && messages[messages.length - 1].special && (
+          <div className="btn">
+            <button
+              onClick={() => handleButtonClick("i want to make new order ")}
+            >
+              Would you like to make an order?
+            </button>
+            <button
+              onClick={() =>
+                handleButtonClick(
+                  "I would like to get a recommendation for a product"
+                )
+              }
+            >
+              Do you want me to recommend you a product?
+            </button>
+            <button
+              onClick={() => handleButtonClick("i want to see your product")}
+            >
+              Do you want to see our products?
+            </button>
+            <button
+              onClick={() =>
+                handleButtonClick("i would like to know your refund policy")
+              }
+            >
+              Do you want to know about our refund policy?
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );

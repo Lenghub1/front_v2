4a7885a9d8f202f0165a7b27ec03f4226a6570f0
LENG <leng@LENGs-MacBook-Pro.local>
2023-11-11 19:37:51 +0700

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Chatbot from './chatbot';
 import './App.css'
-import SocketTest from './notif';
+// import SocketTest from './notif';
 function App() {
   return (
     <div className='screen'>
       <h1> Rukhak bot</h1>
       <Chatbot />
-      <SocketTest />
+      {/* <SocketTest /> */}
     </div>
   );
 }
--- src/Notif.jsx
+++ src/Notif.jsx
@@ -1,87 +1,87 @@
-import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
+// import React, { useEffect, useState } from "react";
+// // import io from "socket.io-client";
 
-const SocketTest = () => {
-  const [buyerNotifications, setBuyerNotifications] = useState([]);
-  const [sellerNotifications, setSellerNotifications] = useState([]);
-  const [showBuyerNotifications, setShowBuyerNotifications] = useState(false);
-  const [showSellerNotifications, setShowSellerNotifications] = useState(false);
+// const SocketTest = () => {
+//   const [buyerNotifications, setBuyerNotifications] = useState([]);
+//   const [sellerNotifications, setSellerNotifications] = useState([]);
+//   const [showBuyerNotifications, setShowBuyerNotifications] = useState(false);
+//   const [showSellerNotifications, setShowSellerNotifications] = useState(false);
 
-  // useEffect(() => {
-  //   const socket = io("http://localhost:4000"); // Replace with your server URL
+//   // useEffect(() => {
+//   //   const socket = io("http://localhost:4000"); // Replace with your server URL
 
-  //   socket.on("connect", () => {
-  //     console.log("Connected to the server via WebSocket");
-  //   });
+//   //   socket.on("connect", () => {
+//   //     console.log("Connected to the server via WebSocket");
+//   //   });
 
-  //   socket.on("disconnect", () => {
-  //     console.log("Disconnected from the server via WebSocket");
-  //   });
+//   //   socket.on("disconnect", () => {
+//   //     console.log("Disconnected from the server via WebSocket");
+//   //   });
 
-  //   socket.on("buyerNotification", (data) => {
-  //     console.log("Product stored:", data.message);
-  //     setBuyerNotifications((prevNotifications) => [...prevNotifications, data.message]);
-  //   });
+//   //   socket.on("buyerNotification", (data) => {
+//   //     console.log("Product stored:", data.message);
+//   //     setBuyerNotifications((prevNotifications) => [...prevNotifications, data.message]);
+//   //   });
 
-  //   socket.on("sellerNotification", (data) => {
-  //     console.log("Seller notification:", data.message);
-  //     setSellerNotifications((prevNotifications) => [...prevNotifications, data.message]);
-  //   });
+//   //   socket.on("sellerNotification", (data) => {
+//   //     console.log("Seller notification:", data.message);
+//   //     setSellerNotifications((prevNotifications) => [...prevNotifications, data.message]);
+//   //   });
 
-  //   return () => {
-  //     socket.disconnect();
-  //   };
-  // }, []);
+//   //   return () => {
+//   //     socket.disconnect();
+//   //   };
+//   // }, []);
 
-  const toggleBuyerNotifications = () => {
-    setShowBuyerNotifications(!showBuyerNotifications);
-  };
+//   const toggleBuyerNotifications = () => {
+//     setShowBuyerNotifications(!showBuyerNotifications);
+//   };
 
-  const toggleSellerNotifications = () => {
-    setShowSellerNotifications(!showSellerNotifications);
-  };
+//   const toggleSellerNotifications = () => {
+//     setShowSellerNotifications(!showSellerNotifications);
+//   };
 
-  return (
-    <div>
-      <h2>Socket.IO Notifications</h2>
-      <div className={`noti ${showBuyerNotifications ? 'show-notifications' : ''}`} onClick={toggleBuyerNotifications}>
-        {showBuyerNotifications ? (
-          <i className="fa-solid fa-bell"></i>
-        ) : (
-          <div style={{ position: "relative" }}>
-            <i className="fa-regular fa-bell"></i>
-            {buyerNotifications.length > 0 && (
-              <div className="notification-dot"></div>
-            )}
-          </div>
-        )}
-        <ul className="notification-list">
-          {buyerNotifications.map((notification, index) => (
-            <li key={index}>{notification}</li>
-          ))}
-        </ul>
-      </div>
-      <div className="sellernoti">
-        <div className={`noti ${showSellerNotifications ? 'show-notifications' : ''}`} onClick={toggleSellerNotifications}>
-          {showSellerNotifications ? (
-            <i className="fa-solid fa-bell"></i>
-          ) : (
-            <div style={{ position: "relative" }}>
-              <i className="fa-regular fa-bell"></i>
-              {sellerNotifications.length > 0 && (
-                <div className="notification-dot"></div>
-              )}
-            </div>
-          )}
-          <ul className="notification-list">
-            {sellerNotifications.map((notification, index) => (
-              <li key={index}>{notification}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Socket.IO Notifications</h2>
+//       <div className={`noti ${showBuyerNotifications ? 'show-notifications' : ''}`} onClick={toggleBuyerNotifications}>
+//         {showBuyerNotifications ? (
+//           <i className="fa-solid fa-bell"></i>
+//         ) : (
+//           <div style={{ position: "relative" }}>
+//             <i className="fa-regular fa-bell"></i>
+//             {buyerNotifications.length > 0 && (
+//               <div className="notification-dot"></div>
+//             )}
+//           </div>
+//         )}
+//         <ul className="notification-list">
+//           {buyerNotifications.map((notification, index) => (
+//             <li key={index}>{notification}</li>
+//           ))}
+//         </ul>
+//       </div>
+//       <div className="sellernoti">
+//         <div className={`noti ${showSellerNotifications ? 'show-notifications' : ''}`} onClick={toggleSellerNotifications}>
+//           {showSellerNotifications ? (
+//             <i className="fa-solid fa-bell"></i>
+//           ) : (
+//             <div style={{ position: "relative" }}>
+//               <i className="fa-regular fa-bell"></i>
+//               {sellerNotifications.length > 0 && (
+//                 <div className="notification-dot"></div>
+//               )}
+//             </div>
+//           )}
+//           <ul className="notification-list">
+//             {sellerNotifications.map((notification, index) => (
+//               <li key={index}>{notification}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default SocketTest;
+// export default SocketTest;

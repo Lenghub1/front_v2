b33cab1177b7a218706e36a0d32c09f7ace2e502
Tourleng <hourleng9@gmail.com>
2023-11-13 08:25:34 +0700

    new

--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -29,7 +29,7 @@ const Chatbot = () => {
 
     try {
       const response = await Axios.post(
-        "https://spotless-toad-coveralls.cyclic.app/api/dialogflow/textQuery",
+        "http://localhost:4000/api/dialogflow/textQuery",
         {
           text: text,
         }
@@ -104,7 +104,6 @@ const Chatbot = () => {
         scrollToBottom();
       }
 
-   
       setInput("");
     } catch (error) {
       const errorMessage = "Error, please check your request";
